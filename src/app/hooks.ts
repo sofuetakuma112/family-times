38d1ae33e3baccaa66221163c2bbdd1f7c4a5ea5
sofuetakuma112 <65995763+sofuetakuma112@users.noreply.github.com>
2026-05-05 16:20:09 +0900

    refactor: 旧Reactクライアントを削除

--- src/app/hooks.ts
+++
@@ -1,7 +0,0 @@
-// import { TypedUseSelectorHook, useDispatch } from "react-redux";
-// import { AppDispatch, RootState } from "./store";
-// import { useSelector } from "react-redux";
-
-// //() => AppDispatch型
-// export const useAppDispatch: () => AppDispatch = useDispatch
-// export const useAppSelector: TypedUseSelectorHook<RootState>= useSelector
